130fc8c49f0f88660cd221c1a08de598c3553a31
DominiqueMarcil <madisonmarcil001@gmail.com>
2024-06-11 11:51:55 -0600

    Added Files

---
+++ script.js
@@ -0,0 +1,35 @@
+// function toggleAccordion(event) {
+//     const header = event.currentTarget;
+//     const content = header.nextElementSibling;
+    
+//     const allContents = document.querySelectorAll('.accordion-content');
+    
+//     // Close all contents
+//     allContents.forEach(item => {
+//         if (item !== content) {
+//             item.style.display = 'none';
+//         }
+//     });
+    
+//     // Toggle current content
+//     if (content.style.display === 'block') {
+//         content.style.display = 'none';
+//     } else {
+//         content.style.display = 'block';
+//     }
+// }
+
+// var modal = document.getElementById("myModal");
+// var img = document.getElementById("Work1, Work2");
+// var modalImg = document.getElementById("img01, img02");
+
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+// }
+
+// var span = document.getElementsByClassName("close")[0];
+
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
