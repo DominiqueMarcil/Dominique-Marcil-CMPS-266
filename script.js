e15a345d779de2eb40eaa3a3509ac9556ed8a501
DominiqueMarcil <madisonmarcil001@gmail.com>
2024-06-18 14:55:29 -0600

    Javascript

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-// function toggleAccordion(event) {
-//     const header = event.currentTarget;
-//     const content = header.nextElementSibling;
+function toggleAccordion(event) {
+    const header = event.currentTarget;
+    const content = header.nextElementSibling;
     
-//     const allContents = document.querySelectorAll('.accordion-content');
+    const allContents = document.querySelectorAll('.accordion-content');
     
-//     // Close all contents
-//     allContents.forEach(item => {
-//         if (item !== content) {
-//             item.style.display = 'none';
-//         }
-//     });
+    // Close all contents
+    allContents.forEach(item => {
+        if (item !== content) {
+            item.style.display = 'none';
+        }
+    });
     
-//     // Toggle current content
-//     if (content.style.display === 'block') {
-//         content.style.display = 'none';
-//     } else {
-//         content.style.display = 'block';
-//     }
-// }
+    // Toggle current content
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+    } else {
+        content.style.display = 'block';
+    }
+}
 
-// var modal = document.getElementById("myModal");
-// var img = document.getElementById("Work1, Work2");
-// var modalImg = document.getElementById("img01, img02");
+var modal1 = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
 
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-// }
+var img1 = document.getElementById("Work1");
+var img2 = document.getElementById("Work2");
 
-// var span = document.getElementsByClassName("close")[0];
+var modalImg1 = document.getElementById("img01");
+var modalImg2 = document.getElementById("img02");
 
-// span.onclick = function() { 
-//   modal.style.display = "none";
-// }
+img1.onclick = function(){
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+}
+
+img2.onclick = function(){
+  modal2.style.display = "block";
+  modalImg2.src = this.src;
+}
+
+var spans = document.getElementsByClassName("close");
+
+for (var i = 0; i < spans.length; i++) {
+  spans[i].onclick = function() { 
+    this.parentElement.style.display = "none";
+  }
+}
